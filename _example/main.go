878bcca7edb93cdeddcc2921448269c2b5231305
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-07-28 08:52:31 +0800

    docs: update example

--- _example/main.go
+++ _example/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"embed"
+	"encoding/json"
 	"log"
 	"net/http"
 
 	ginI18n "github.com/gin-contrib/i18n"
 	"github.com/gin-gonic/gin"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
 )
+
+//go:embed i18n/localizeJSON/*
+var fs embed.FS
 
 func main() {
 	// new gin engine
@@ -15,7 +21,18 @@ func main() {
 	router := gin.New()
 
 	// apply i18n middleware
-	router.Use(ginI18n.Localize())
+	router.Use(ginI18n.Localize(ginI18n.WithBundle(&ginI18n.BundleCfg{
+		DefaultLanguage:  language.English,
+		FormatBundleFile: "json",
+		AcceptLanguage:   []language.Tag{language.English, language.German, language.Chinese},
+		RootPath:         "./i18n/localizeJSON/",
+		UnmarshalFunc:    json.Unmarshal,
+		// After commenting this line, use defaultLoader
+		// it will be loaded from the file
+		Loader: &ginI18n.EmbedLoader{
+			FS: fs,
+		},
+	})))
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, ginI18n.MustGetMessage(ctx, "welcome"))
