2e4ce067a55577e0f1951c4c3b1a1459fbd65e27
appleboy <appleboy.tw@gmail.com>
2024-11-09 11:41:13 +0800

    docs: enhance code documentation for ginI18n implementation
    - Add detailed comments for the `ginI18nImpl` struct and its methods
    - Add detailed comments for the `GinI18n` interface and its methods
    - Add detailed comments for the `GetLngHandler` and `Option` types
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- interface.go
+++ interface.go
@@ -5,11 +5,21 @@ import (
 	"golang.org/x/text/language"
 )
 
-// GinI18n ...
+// GinI18n is an interface that defines methods for internationalization (i18n) in a Gin web framework context.
+// It provides methods to get localized messages and configure the i18n bundle and language handler.
 type GinI18n interface {
+	// GetMessage retrieves a localized message based on the provided context and parameter.
+	// It returns the localized message as a string and an error if the message could not be retrieved.
 	GetMessage(context *gin.Context, param interface{}) (string, error)
+
+	// MustGetMessage retrieves a localized message based on the provided context and parameter.
+	// It returns the localized message as a string and panics if the message could not be retrieved.
 	MustGetMessage(context *gin.Context, param interface{}) string
+
+	// SetBundle sets the i18n bundle configuration.
 	SetBundle(cfg *BundleCfg)
+
+	// SetGetLngHandler sets the handler function to determine the language from the context.
 	SetGetLngHandler(handler GetLngHandler)
 	hasLang(language string) bool
 	getDefaultLanguage() language.Tag
--- type.go
+++ type.go
@@ -3,9 +3,10 @@ package i18n
 import "github.com/gin-gonic/gin"
 
 type (
-	// GetLngHandler ...
+	// Option is a function type that takes a GinI18n instance and applies a configuration to it.
+	// It is used to customize the behavior of the GinI18n middleware.
 	GetLngHandler = func(context *gin.Context, defaultLng string) string
 
-	// Option ...
+	// Option is a function type that takes a GinI18n instance and applies a configuration to it.
 	Option func(GinI18n)
 )
